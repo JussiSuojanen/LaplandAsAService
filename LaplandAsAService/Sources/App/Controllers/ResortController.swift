70cc02b5895127be128742f4d6819ecf93fdc6ae
Jussi Suojanen <jussi.suojanen@vincit.fi>
2017-03-04 13:19:37 +0900

    Implement ResortController
    - Add and list resorts available

--- LaplandAsAService/Sources/App/Controllers/ResortController.swift
+++ LaplandAsAService/Sources/App/Controllers/ResortController.swift
@@ -6,4 +6,34 @@
 //
 //
 
-import Foundation
+import Vapor
+import HTTP
+import VaporMySQL
+
+final class ResortController: ResourceRepresentable {
+
+    func index(request: Request) throws -> ResponseRepresentable {
+        return try Resort.all().makeNode().converted(to: JSON.self)
+    }
+
+    func create(request: Request) throws -> ResponseRepresentable {
+        var resort = try request.resort()
+        try resort.save()
+        return resort
+    }
+
+    func makeResource() -> Resource<Resort> {
+        return Resource(index: index,
+                        store: create)
+    }
+}
+
+extension Request {
+    func resort() throws -> Resort {
+        guard let json = json else {
+            throw Abort.badRequest
+        }
+
+        return try Resort(node: json)
+    }
+}
