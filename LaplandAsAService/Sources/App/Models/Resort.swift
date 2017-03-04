065497db305900e27382e7acb1afc27f2d24b228
Jussi Suojanen <jussi.suojanen@vincit.fi>
2017-03-04 11:22:16 +0900

    Add MySQL provider and  model for resorts

--- LaplandAsAService/Sources/App/Models/Resort.swift
+++ LaplandAsAService/Sources/App/Models/Resort.swift
@@ -6,4 +6,71 @@
 //
 //
 
+import Vapor
+import HTTP
+import Fluent
 import Foundation
+
+final class Resort: Model {
+    var id: Node?
+    var exists: Bool = false
+    var name: String
+    var height: Int
+    var numberOfSlopes: Int
+    var numberOfSlopesOpen: Int
+    var crossCountrySki: Int
+    var defaultImage: String
+    var type: Int
+
+    //Convenience init
+    init(name: String, height: Int, numberOfSlopes: Int, numberOfSlopesOpen: Int, crossCountrySki: Int, defaultImage:String, type: Int) {
+        self.name = name
+        self.height = height
+        self.numberOfSlopes = numberOfSlopes
+        self.numberOfSlopesOpen = numberOfSlopesOpen
+        self.crossCountrySki = crossCountrySki
+        self.defaultImage = defaultImage
+        self.type = type
+    }
+
+    init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+        name = try node.extract("name")
+        height = try node.extract("height")
+        numberOfSlopes = try node.extract("numberOfSlopes")
+        numberOfSlopesOpen = try node.extract("numberOfSlopesOpen")
+        crossCountrySki = try node.extract("crossCountrySki")
+        defaultImage = try node.extract("defaultImage")
+        type = try node.extract("type")
+    }
+
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+            "id": id,
+            "name": name,
+            "height": height,
+            "numberOfSlopes": numberOfSlopes,
+            "numberOfSlopesOpen": numberOfSlopesOpen,
+            "crossCountrySki": crossCountrySki,
+            "defaultImage": defaultImage,
+            "type": type
+            ])
+    }
+
+    static func prepare(_ database: Database) throws {
+        try database.create("resorts") { resorts in
+            resorts.id()
+            resorts.string("name")
+            resorts.string("height")
+            resorts.string("numberOfSlopes")
+            resorts.string("numberOfSlopesOpen")
+            resorts.string("crossCountrySki")
+            resorts.string("defaultImage")
+            resorts.string("type")
+        }
+    }
+
+    static func revert(_ database: Database) throws {
+        try database.delete("resorts")
+    }
+}
--- LaplandAsAService/Sources/App/main.swift
+++ LaplandAsAService/Sources/App/main.swift
@@ -1,5 +1,6 @@
 import Vapor
+import VaporMySQL
 
-let drop = Droplet()
+let drop = Droplet(preparations: [Resort.self], providers: [VaporMySQL.Provider.self])
 
 drop.run()
