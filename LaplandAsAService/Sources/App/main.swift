69fbf8381bf9c0665b31ab1caad4dc32728bdf26
Jussi Suojanen <jussi.suojanen@vincit.fi>
2017-03-04 13:21:51 +0900

    Add routes for creation and listing resorts

--- LaplandAsAService/Sources/App/main.swift
+++ LaplandAsAService/Sources/App/main.swift
@@ -3,4 +3,9 @@ import VaporMySQL
 
 let drop = Droplet(preparations: [Resort.self], providers: [VaporMySQL.Provider.self])
 
+let resortController = ResortController()
+
+drop.resource("listResorts", resortController)
+drop.resource("addResort", resortController)
+
 drop.run()
